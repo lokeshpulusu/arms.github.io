3d01d81b5b9a78653c7b7d0517666f8b67fefe7f
lokeshpulusu <98742455+lokeshpulusu@users.noreply.github.com>
2023-03-11 13:14:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # Check if username and password are correct
+    if username == "myuser" and password == "mypassword":
+        return "Login successful!"
+    else:
+        return "Invalid username or password"
 
 if __name__ == "__main__":
     app.run()
